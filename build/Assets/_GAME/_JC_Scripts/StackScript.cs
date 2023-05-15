010490bea7ae28412c731bfda5f20d95cdcb951c
Hari Harsha <hari33841@gmail.com>
2023-05-15 16:44:09 +0530

    ** bonus change

--- build/Assets/_GAME/_JC_Scripts/StackScript.cs
+++ build/Assets/_GAME/_JC_Scripts/StackScript.cs
@@ -30,7 +30,7 @@ public class StackScript : MonoBehaviour
             SoundManger.soundctrl.playClip(knifeHit);
             Instantiate(blast, transform.position, Quaternion.Euler(-90, 0, 0));
             //stacks.transform.localPosition -= new Vector3(0, 0.5f, 0);
-            stacks.transform.DOMoveY(stacks.transform.position.y - 0.15f, 0.1f, false);
+            stacks.transform.DOLocalMoveY(stacks.transform.localPosition.y - 0.5f, 0.1f, false);
             if (FindObjectOfType<ButtonManager>().ishaptic)
             {
                 MMVibrationManager.Haptic(HapticTypes.Success);
