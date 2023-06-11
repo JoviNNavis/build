eb4d8651a2cd6cd0d5d7017573b9074354213a5a
Hari Harsha <hari33841@gmail.com>
2023-06-11 12:36:28 +0530

    knife bug..

--- build/Assets/_GAME/_JC_Scripts/KnifeScript.cs
+++ build/Assets/_GAME/_JC_Scripts/KnifeScript.cs
@@ -89,7 +89,7 @@ public class KnifeScript : MonoBehaviour
                
                 _knife = Instantiate(knifemat[randcolors], new Vector3(2.4f, transform.localPosition.y, transform.localPosition.z), Quaternion.identity);
                     FindObjectOfType<FailScript1>().Knifes.Add(_knife.gameObject.transform);
-                knifeCounter.knifeCountValue = FindObjectOfType<FailScript>().Knifes.Count;
+                knifeCounter.knifeCountValue = FindObjectOfType<FailScript1>().Knifes.Count;
                 randcolors++;
                     transform.position += new Vector3(0, 0.7f, 0);
                     newBallPos.transform.position += new Vector3(0, 0.7f, 0);
@@ -114,7 +114,7 @@ public class KnifeScript : MonoBehaviour
               
                 fireRate = 14;
 
-                knifeCounter.knifeCountValue = FindObjectOfType<FailScript>().Knifes.Count;
+                knifeCounter.knifeCountValue = FindObjectOfType<FailScript1>().Knifes.Count;
 
 
                 combovalue = 1;
